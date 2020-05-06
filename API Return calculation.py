2680f8a2a24d9872b7665aec0fadd14e9c327047
Purvansh Shah <purvansh11@gmail.com>
2020-05-07 00:22:40 +0530

    Added features

--- API Return calculation.py
+++ API Return calculation.py
@@ -1,26 +1,28 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
 
 
 import quandl
-
+#Configuration key for Quandl from where data is accessed
 quandl.ApiConfig.api_key='RZCxWTQxvxZLMAzP22Ay'
 
+#For sample case, start_date and end_date are pre defined
 limited_dt=quandl.get('AMFI/140249', start_date='2019-07-03', end_date='2019-07-03')
 print("LIVE API DATA...")
 print(limited_dt)
 print()
-yr=input("Enter year span: ")
+yr=int(input("Enter year span (non-zero) : "))
 
-print("Displaying the expected return")
+print("Displaying the expected return...")
 
-retur=(limited_dt['Net Asset Value'][0]+limited_dt['Net Asset Value'][0]*0.1)
+#Applying hypothetical formula on the extracted values
+retur=(limited_dt['Net Asset Value'][0]+limited_dt['Net Asset Value'][0]*0.1/yr)
+
+#printing the final result
 print(retur)
 
 
-# In[ ]:
 
 
 
